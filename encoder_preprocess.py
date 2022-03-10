cfd625386b35ddbe41e413c32e464276490e318d
Daniil Lyakhov <daniil.lyakhov@intel.com>
2022-03-10 14:16:28 +0300

    Improve encoder preprocessor / clustering init

--- encoder_preprocess.py
+++ encoder_preprocess.py
@@ -1,13 +1,9 @@
 import argparse
 
-from functools import partial
 from utils.argutils import print_args
 from pathlib import Path
 
-from encoder.preprocess import (preprocess_librispeech,
-                                preprocess_voxceleb1,
-                                preprocess_voxceleb2,
-                                preprocess_like_librispeech)
+from encoder.preprocess import DatasetsPreprocessor
 
 
 if __name__ == "__main__":
@@ -40,11 +36,20 @@ if __name__ == "__main__":
         "Comma-separated list of the name of the datasets you want to preprocess. Only the train "
         "set of these datasets will be used. Possible names: librispeech_other, voxceleb1, "
         "voxceleb2.")
+    parser.add_argument("-e", "--encoder_model_fpath", type=Path,
+                        default="saved_models/default/encoder.pt", help= \
+                            "Path your trained encoder model.")
+    parser.add_argument("--mode", type=str, default='to_mels', help= \
+                        "mode of preprocessing. Could be from [`to_mels`,`clustering`]."
+                        "`to_mels` means mels is outputted after preprocessing."
+                        "`clustering` means embeddings is outputted after preprocessing.")
     parser.add_argument("-s", "--skip_existing", action="store_true", help=\
         "Whether to skip existing output files with the same name. Useful if this script was "
         "interrupted.")
     parser.add_argument("--no_trim", action="store_true", help=\
         "Preprocess audio without trimming silences (not recommended).")
+    parser.add_argument("--n_processes", type=int, default=8, help= \
+        "Number of process for preprocessing.")
     args = parser.parse_args()
 
     # Verify webrtcvad is available
@@ -58,7 +63,6 @@ if __name__ == "__main__":
     del args.no_trim
 
     # Process the arguments
-    args.datasets = args.datasets.split(",")
     if not hasattr(args, "out_dir"):
         args.out_dir = args.datasets_root.joinpath("SV2TTS", "encoder")
     assert args.datasets_root.exists()
@@ -66,14 +70,5 @@ if __name__ == "__main__":
 
     # Preprocess the datasets
     print_args(args, parser)
-    preprocess_func = {
-        "librispeech_other": preprocess_librispeech,
-        "voxceleb1": preprocess_voxceleb1,
-        "voxceleb2": preprocess_voxceleb2,
-        "book": partial(preprocess_like_librispeech, 'book'),
-        "mozilla": partial(preprocess_like_librispeech, 'mozilla'),
-    }
     args = vars(args)
-    for dataset in args.pop("datasets"):
-        print("Preprocessing %s" % dataset)
-        preprocess_func[dataset](**args)
+    DatasetsPreprocessor(**args).preprocess()
